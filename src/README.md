7ddf7a2123be2cb7cd8700f461aa5d8ea3d5b69c
Hector <hector.it.dev@gmail.com>
2022-01-26 10:36:50 +0100

    initial commit

---
+++ src/README.md
@@ -0,0 +1,8 @@
+# Introduction
+This is a small console application to remove form fields from a PDF file.
+It leaves the original PDF unmodified, but creates a second pdf which is flattened
+
+It leverages IText7 whih has an AGPL license.
+
+# Instructions
+Simple change the variable paths to what you need
---
+++ src/pdf-flattener/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using iText.Forms;
+using iText.Kernel.Pdf;
+var inputFilePath = @""; // replace with the path to the input pdf
+var outputFilePath = @""; // replace with the path where the flattened pdf should be saved to
+var pdfFile = File.OpenRead(inputFilePath);
+var reader = new PdfReader(pdfFile);
+var pdfWriter = new PdfWriter(outputFilePath);
+var pdfDoc = new PdfDocument(reader, pdfWriter);
+var acroForm = PdfAcroForm.GetAcroForm(pdfDoc, false);
+acroForm.FlattenFields();
+pdfDoc.Close();
